00f28be8a256f6270de40e4ef540e09522c4f048
aftabmaner-27 <aftabmaner3131@gmail.com>
2022-09-17 14:47:24 +0530

    Check Internet Connection Continuously in Android

---
+++ app/src/main/java/com/aftabmaner27/checkinternet/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.aftabmaner27.checkinternet.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
